a2e7a3824c327f6629ab82164c0c25945212ecb8
Katharina <hi_adresse@gmx.de>
2016-12-10 08:24:07 +0100

    ch12 - end of chapter

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Katharina on 10.12.2016.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    
+}
